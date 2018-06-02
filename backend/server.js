d91fc6fadea301629e1eb1f554fca8c79563943c
Anna Ågren <info@annaagren.se>
2018-06-02 16:51:30 +0200

    Revert "no more css"
    This reverts commit de02477bbd8cfb90e1700e4a3c50bff553583628.

--- backend/server.js
+++ backend/server.js
@@ -15,22 +15,6 @@ mongoose.Promise = Promise
 mongoose.connection.on("error", err => console.error("Connection error:", err))
 mongoose.connection.once("open", () => console.log("Connected to mongodb"))
 
-const Restaurant = mongoose.model("Restaurant", {
-  id: Number,
-  name: String,
-  type: String,
-  lat: Number,
-  long: Number,
-  area: String,
-  description: String,
-  phoneNumber: String,
-  openingHours: String,
-  address: String,
-  homepage: String
-})
-
-
-
 	app.get("/", (req, res) =>
  	 res.send("Hello World!")
 	)
