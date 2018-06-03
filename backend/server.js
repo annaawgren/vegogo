a461fefaf3521299356a4dd132a291525d39a951
Pär Thernström <par.thernstrom@gmail.com>
2018-06-03 17:58:38 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,22 @@
-import express from "express"
-import mongoose from "mongoose"
-import bodyParser from "body-parser"
-import cors from "cors"
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
 
+const app = express();
 
-const app = express()
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use(bodyParser.json())
-app.use(cors())
+mongoose.connect(
+  "mongodb://localhost/8080",
+  { useMongoClient: true }
+);
+mongoose.Promise = Promise;
 
-mongoose.connect("mongodb://localhost/8080", { useMongoClient: true })
-mongoose.Promise = Promise
+mongoose.connection.on("error", err => console.error("Connection error:", err));
+mongoose.connection.once("open", () => console.log("Connected to mongodb"));
 
-mongoose.connection.on("error", err => console.error("Connection error:", err))
-mongoose.connection.once("open", () => console.log("Connected to mongodb"))
+app.get("/", (req, res) => res.send("Hello World!"));
 
-	app.get("/", (req, res) =>
- 	 res.send("Hello World!")
-	)
-
-	app.listen(8080, () =>
-  	console.log("Example app listening on port 8080!")
-	)
+app.listen(8080, () => console.log("Example app listening on port 8080!"));
