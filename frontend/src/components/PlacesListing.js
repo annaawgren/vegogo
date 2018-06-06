ad95067958bcbffe1a9a13e83be2dc26ccc47af6
Pär Thernström <par.thernstrom@gmail.com>
2018-06-06 23:13:10 +0200

    Add map and other stuff to the place listing

--- frontend/src/components/PlacesListing.js
+++ frontend/src/components/PlacesListing.js
@@ -1,14 +1,64 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { StaticGoogleMap, Marker, Path } from "react-static-google-map";
 
 class PlacesListing extends Component {
   render() {
     let { headline, places } = this.props;
-
     let placesItems;
+
     if (places) {
       placesItems = places.map(place => {
-        return <li key={place.slug}>{place.name}</li>;
+        let {
+          name,
+          slug,
+          budget,
+          location,
+          content,
+          foodTypes,
+          foodTimes
+        } = place;
+
+        let types = foodTypes.map(type => <li key={type.key}>{type.name}</li>);
+        if (types) {
+          types = <ul>{types}</ul>;
+        }
+
+        let times = foodTimes.map(type => <li key={type.key}>{type.name}</li>);
+        if (times) {
+          times = <ul>{times}</ul>;
+        }
+
+        return (
+          <li key={slug}>
+            <Link to={slug}>{name}</Link>
+            <p>{budget}</p>
+            {types}
+            {times}
+            {location && <p>location.street1</p>}
+            {location &&
+              location.geo && (
+                <div>
+                  <p>{location.geo[0]}</p>
+                  <p>{location.geo[1]}</p>
+                  {/* https://www.npmjs.com/package/react-static-google-map */}
+                  <StaticGoogleMap size="400x250" zoom="15">
+                    <Marker
+                      location={{ lat: location.geo[1], lng: location.geo[0] }}
+                      color="green"
+                      label="V"
+                    />
+                  </StaticGoogleMap>
+                </div>
+              )}
+            {content && (
+              <div dangerouslySetInnerHTML={{ __html: content.brief }} />
+            )}
+          </li>
+        );
       });
+
+      placesItems = <ul>{placesItems}</ul>;
     }
 
     return (
