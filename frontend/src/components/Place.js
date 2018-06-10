0684305d9288c4d34bc7d1cea96c646b91ec08c8
Pär Thernström <par.thernstrom@gmail.com>
2018-06-10 20:38:48 +0200

    Add place content

--- frontend/src/components/Place.js
+++ frontend/src/components/Place.js
@@ -1,15 +1,106 @@
 import React, { Component } from "react";
 import SiteHeader from "./SiteHeader";
 import SiteFooter from "./SiteFooter";
+import { StaticGoogleMap, Marker } from "react-static-google-map";
+import pinImg from "../images/pin.png";
+import { GOOGLE_MAPS_API_KEY, IMAGES_URL, API_URL } from "../api-config";
 
 class Place extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      place: {}
+    };
+  }
+  componentDidMount() {
+    let placeSlug = this.props.match.params.place;
+    let placesApiUrl = `${API_URL}/place/slug/${placeSlug}`;
+
+    fetch(placesApiUrl)
+      .then(data => {
+        return data.json();
+      })
+      .then(data => {
+        this.setState({ place: data.place });
+      });
+  }
+
   render() {
+    let {
+      name,
+      slug,
+      budget,
+      location,
+      content,
+      foodTypes = [],
+      foodTimes = [],
+      image
+    } = this.state.place;
+
+    let types = foodTypes.map(type => <li key={type.key}>{type.name}</li>);
+    if (types) {
+      types = <ul>{types}</ul>;
+    }
+
+    let times = foodTimes.map(type => <li key={type.key}>{type.name}</li>);
+    if (times) {
+      times = <ul>{times}</ul>;
+    }
+
+    let imageMarkup;
+    if (image) {
+      imageMarkup = (
+        <p>
+          <img
+            src={`${IMAGES_URL}/places/${image.filename}`}
+            alt=""
+            className="PlaceItem-photo-img"
+          />
+        </p>
+      );
+    }
+
     return (
       <div>
         <SiteHeader />
 
         <h1>Place</h1>
         <p>This page shows a single place.</p>
+
+        <article key={slug} className="PlacesListing-placeItem">
+          <h1>{name}</h1>
+          {imageMarkup}
+          <p>{budget}</p>
+          {types}
+          {times}
+          {location && <p>{location.street1}</p>}
+          {location &&
+            location.geo && (
+              <div>
+                <p>{location.geo[0]}</p>
+                <p>{location.geo[1]}</p>
+                {/* https://www.npmjs.com/package/react-static-google-map */}
+                <StaticGoogleMap
+                  size="300x200"
+                  zoom="15"
+                  apiKey={GOOGLE_MAPS_API_KEY}
+                  className="PlacesListing-placeItem-mapImage"
+                >
+                  <Marker
+                    location={{ lat: location.geo[1], lng: location.geo[0] }}
+                    color="green"
+                    label="V"
+                    xiconURL={document.location.origin + pinImg}
+                    xxxiconURL="https://goo.gl/1oTJ9Y"
+                    iconURL="http://beta.vegogo.se/static/media/pin.46fd9ba2.png"
+                  />
+                </StaticGoogleMap>
+              </div>
+            )}
+          {content && (
+            <div dangerouslySetInnerHTML={{ __html: content.brief }} />
+          )}
+        </article>
 
         <SiteFooter />
       </div>
