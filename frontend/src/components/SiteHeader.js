876e8b6185437c84f260389824cfb9b912a46bb2
Pär Thernström <par.thernstrom@gmail.com>
2018-06-18 19:16:25 +0200

    Set state on header on navtoggle click + show a basic non-working nav menu

--- frontend/src/components/SiteHeader.js
+++ frontend/src/components/SiteHeader.js
@@ -3,7 +3,42 @@ import { Link } from "react-router-dom";
 import logoImg from "../images/vegogo-logo.svg";
 import "./SiteHeader.css";
 
+let Navigation = function(props) {
+  return (
+    <nav>
+      <h3>Vegogo Navigation</h3>
+      <ul>
+        <li>
+          <a href="#">nav item one</a>
+        </li>
+        <li>
+          <a href="#">nav item two</a>
+        </li>
+        <li>
+          <a href="#">A short one</a>
+        </li>
+        <li>
+          <a href="#">And a long one, because everything must work, right?</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
 class SiteHeader extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      navOpen: false
+    };
+
+    this.handleNavToggleClick = this.handleNavToggleClick.bind(this);
+  }
+
+  handleNavToggleClick(e) {
+    this.setState({ navOpen: !this.state.navOpen });
+  }
   render() {
     return (
       <header className="SiteHeader">
@@ -13,7 +48,13 @@ class SiteHeader extends Component {
           </Link>
         </h1>
         <p>The new guide* to vegan eating *curated for you with &lt;3</p>
-        <button className="SiteHeader-navToggler">☰</button>
+        <button
+          onClick={this.handleNavToggleClick}
+          className="SiteHeader-navToggler"
+        >
+          ☰
+        </button>
+        {this.state.navOpen && <Navigation />}
       </header>
     );
   }
