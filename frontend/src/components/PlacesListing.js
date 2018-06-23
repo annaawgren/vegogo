13d033e712bb076ba35e715f049df98e0bd27a9e
Pär Thernström <par.thernstrom@gmail.com>
2018-06-23 23:39:22 +0200

    Use Place component in place listing

--- frontend/src/components/PlacesListing.js
+++ frontend/src/components/PlacesListing.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { StaticGoogleMap, Marker } from "react-static-google-map";
-import { GOOGLE_MAPS_API_KEY, IMAGES_URL } from "../api-config";
+// import { Link } from "react-router-dom";
+// import { StaticGoogleMap, Marker } from "react-static-google-map";
+// import { GOOGLE_MAPS_API_KEY, IMAGES_URL } from "../api-config";
 // import pinImg from "../images/pin.png";
 import Place from "./Place";
 import "./PlacesListing.css";
@@ -13,73 +13,11 @@ class PlacesListing extends Component {
 
     if (places) {
       placesItems = places.map(place => {
-        let {
-          name,
-          slug,
-          budget,
-          location,
-          content,
-          foodTypes,
-          foodTimes,
-          image
-        } = place;
-
-        let types = foodTypes.map(type => <li key={type.key}>{type.name}</li>);
-        if (types) {
-          types = <ul>{types}</ul>;
-        }
-
-        let times = foodTimes.map(type => <li key={type.key}>{type.name}</li>);
-        if (times) {
-          times = <ul>{times}</ul>;
-        }
-
-        let imageMarkup;
-        if (image) {
-          imageMarkup = (
-            <p>
-              <img
-                src={`${IMAGES_URL}/places/${image.filename}`}
-                alt=""
-                className="PlaceItem-photo-img"
-              />
-            </p>
-          );
-        }
+        let { slug } = place;
 
         return (
           <li key={slug} className="PlacesListing-placeItem">
-            <h1>
-              <Link to={`/place/${slug}`}>{name}</Link>
-            </h1>
-            {imageMarkup}
-            <p>{budget}</p>
-            {types}
-            {times}
-            {location && <p>{location.street1}</p>}
-            {location &&
-              location.geo && (
-                <div>
-                  {/* https://www.npmjs.com/package/react-static-google-map */}
-                  <StaticGoogleMap
-                    size="300x200"
-                    zoom="15"
-                    scale="2"
-                    apiKey={GOOGLE_MAPS_API_KEY}
-                    className="PlacesListing-placeItem-mapImage"
-                  >
-                    <Marker
-                      location={{ lat: location.geo[1], lng: location.geo[0] }}
-                      color="green"
-                      label="V"
-                      iconURL="https://beta.vegogo.se/favicon-32x32.png"
-                    />
-                  </StaticGoogleMap>
-                </div>
-              )}
-            {content && (
-              <div dangerouslySetInnerHTML={{ __html: content.brief }} />
-            )}
+            <Place slug={slug} />
           </li>
         );
       });
