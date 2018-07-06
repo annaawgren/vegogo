090102f19003b2b33054cfbf428ad601cbce75d6
Pär Thernström <par.thernstrom@gmail.com>
2018-07-06 18:45:09 +0200

    Add API endpoints for areas

--- keystone/routes/api/areas.js
+++ keystone/routes/api/areas.js
@@ -1,7 +1,7 @@
 var async = require("async");
 var keystone = require("keystone");
 var cloudinary = require("cloudinary");
-var Place = keystone.list("Place");
+var Area = keystone.list("Area");
 var apiConfig = require("../../api-config");
 
 /**
@@ -9,23 +9,8 @@ var apiConfig = require("../../api-config");
  * https://gist.github.com/JedWatson/9741171
  */
 
-function getPlaceImage(place) {
-	let imageUrl = `${apiConfig.IMAGES_URL}/places/${place.image.filename}`;
-	let image = cloudinary.url(imageUrl, {
-		type: "fetch",
-		secure: true,
-		width: 150,
-		height: 150,
-		crop: "thumb",
-		gravity: "face",
-		radius: 20
-	});
-
-	return image;
-}
-
 /**
- * List Places
+ * List Areas
  */
 exports.list = function(req, res) {
 	let sortParam = req.query.sort || "published";
@@ -40,22 +25,22 @@ exports.list = function(req, res) {
 			sort = { publishedDate: -1 };
 	}
 
-	Place.model
+	Area.model
 		.find()
 		.sort(sort)
-		.populate("foodTimes foodTypes")
+		// .populate("foodTimes foodTypes")
 		.exec(function(err, items) {
 			if (err) return res.apiError("database error", err);
 
-			items = items.map(place => {
-				let imageThumb = place.vImageThumb;
-				place = place.toJSON();
-				place.imageThumb = imageThumb;
-				return place;
-			});
+			// items = items.map(place => {
+			// 	let imageThumb = place.vImageThumb;
+			// 	place = place.toJSON();
+			// 	place.imageThumb = imageThumb;
+			// 	return place;
+			// });
 
 			res.apiResponse({
-				places: items
+				areas: items
 			});
 		});
 };
@@ -65,15 +50,15 @@ exports.list = function(req, res) {
  * http://localhost:3131/api/place/id/5b158f4b16474ee5772d1113
  */
 exports.getId = function(req, res) {
-	Place.model
+	Area.model
 		.findById(req.params.id)
-		.populate("foodTimes foodTypes")
+		// .populate("foodTimes foodTypes")
 		.exec(function(err, item) {
 			if (err) return res.apiError("database error", err);
 			if (!item) return res.apiError("not found");
 
 			res.apiResponse({
-				place: item
+				area: item
 			});
 		});
 };
@@ -83,17 +68,17 @@ exports.getId = function(req, res) {
  * http://localhost:3131/api/place/slug/minh-mat
  */
 exports.getSlug = function(req, res) {
-	Place.model
+	Area.model
 		.findOne({ slug: req.params.slug })
-		.populate("foodTimes foodTypes")
+		// .populate("foodTimes foodTypes")
 		.exec(function(err, item) {
 			if (err) return res.apiError("database error", err);
 			if (!item) return res.apiError("not found");
 
-			console.log("imageThumb", item.imageThumb);
+			// console.log("imageThumb", item.imageThumb);
 
 			res.apiResponse({
-				place: item
+				area: item
 			});
 		});
 };
