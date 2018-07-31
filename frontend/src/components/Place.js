c3452241684c42714d182b7404d9972612d4353c
Pär Thernström <par.thernstrom@gmail.com>
2018-07-31 20:34:01 +0200

    Move place map and location to own component

--- frontend/src/components/Place.js
+++ frontend/src/components/Place.js
@@ -54,6 +54,70 @@ function PlaceTimes({ foodTimes }) {
   return times;
 }
 
+class PlaceLocation extends Component {
+  render() {
+    let { location, phone, homepage, name } = this.props;
+    let homepageOut = null;
+    let locationAndMap = null;
+    let { homepagePresentation, homepageWithProtocol } = cleanupHomepage(
+      homepage
+    );
+
+    if (homepagePresentation && homepageWithProtocol) {
+      homepageOut = (
+        <p className="PlaceItem-meta-item">
+          <a target="_blank" rel="noopener" href={homepageWithProtocol}>
+            {homepagePresentation}
+          </a>
+        </p>
+      );
+    }
+
+    if (location && location.geo) {
+      let googleLink = `http://maps.google.com/?q=${name}, ${
+        location.street1
+      }, ${location.state}, ${location.country}`;
+
+      locationAndMap = (
+        <div className="PlaceItem-meta">
+          <p className="PlaceItem-meta-item">
+            <a target="_blank" ref="noopener" href={googleLink}>
+              {location.street1}
+            </a>
+          </p>
+          {phone && (
+            <p className="PlaceItem-meta-item">
+              <a href={`tel:${phone}`}>{phone}</a>
+            </p>
+          )}
+          {homepageOut}
+          {/* https://www.npmjs.com/package/react-static-google-map */}
+          <p className="PlaceItem-staticMap">
+            <a href={googleLink} target="_blank" rel="noopener">
+              <StaticGoogleMap
+                size="300x200"
+                zoom="15"
+                scale="2"
+                apiKey={GOOGLE_MAPS_API_KEY}
+                className="PlaceItem-meta-item PlacesListing-placeItem-mapImage"
+              >
+                <Marker
+                  location={{ lat: location.geo[1], lng: location.geo[0] }}
+                  color="green"
+                  label="V"
+                  iconURL="https://beta.vegogo.se/favicon-32x32.png"
+                />
+              </StaticGoogleMap>
+            </a>
+          </p>
+        </div>
+      );
+    }
+
+    return locationAndMap;
+  }
+}
+
 /**
  * Place can get what to render from a slug + props with full place object, for example when being used in a listing
  * or from props match when viewing a place permalink.
@@ -177,62 +241,6 @@ class Place extends Component {
       );
     }
 
-    let homepageOut;
-    let { homepagePresentation, homepageWithProtocol } = cleanupHomepage(
-      homepage
-    );
-    if (homepagePresentation && homepageWithProtocol) {
-      homepageOut = (
-        <p className="PlaceItem-meta-item">
-          <a target="_blank" rel="noopener" href={homepageWithProtocol}>
-            {homepagePresentation}
-          </a>
-        </p>
-      );
-    }
-
-    let locationAndMap;
-    if (location && location.geo) {
-      let googleLink = `http://maps.google.com/?q=${name}, ${
-        location.street1
-      }, ${location.state}, ${location.country}`;
-
-      locationAndMap = (
-        <div className="PlaceItem-meta">
-          <p className="PlaceItem-meta-item">
-            <a target="_blank" ref="noopener" href={googleLink}>
-              {location.street1}
-            </a>
-          </p>
-          {phone && (
-            <p className="PlaceItem-meta-item">
-              <a href={`tel:${phone}`}>{phone}</a>
-            </p>
-          )}
-          {homepageOut}
-          {/* https://www.npmjs.com/package/react-static-google-map */}
-          <p className="PlaceItem-staticMap">
-            <a href={googleLink} target="_blank" rel="noopener">
-              <StaticGoogleMap
-                size="300x200"
-                zoom="15"
-                scale="2"
-                apiKey={GOOGLE_MAPS_API_KEY}
-                className="PlaceItem-meta-item PlacesListing-placeItem-mapImage"
-              >
-                <Marker
-                  location={{ lat: location.geo[1], lng: location.geo[0] }}
-                  color="green"
-                  label="V"
-                  iconURL="https://beta.vegogo.se/favicon-32x32.png"
-                />
-              </StaticGoogleMap>
-            </a>
-          </p>
-        </div>
-      );
-    }
-
     let placeClassNames = classnames({
       placeItem: true,
       "PlaceItem--isSingleView": isSingleView,
@@ -294,7 +302,7 @@ class Place extends Component {
               <PlaceTimes foodTimes={foodTimes} />
             </div>
             {contentOut}
-            {locationAndMap}
+            <PlaceLocation {...{ location, phone, name, homepage }} />
             {imagesMarkup}
           </div>
         )}
