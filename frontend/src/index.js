25b3dfde1f5aed9fafa0a255815f7f604d9b2ab6
Pär Thernström <par.thernstrom@gmail.com>
2018-10-06 14:43:34 +0200

    Display Lint Output in the Editor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import "react-app-polyfill/ie9"; // For IE 9-11 support
 import React from "react";
+// import Component from "react";
 import ReactDOM from "react-dom";
 // import { render } from 'react-snapshot';
 import "./index.scss";
